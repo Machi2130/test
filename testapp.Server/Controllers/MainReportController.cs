1408497731d948b488b3c98c245b46952c35822e
Machi2130 <117148639+Machi2130@users.noreply.github.com>
2025-11-21 12:49:39 +0530

    Update MainReportController.cs

--- testapp.Server/Controllers/MainReportController.cs
+++ testapp.Server/Controllers/MainReportController.cs
@@ -1,25 +1,27 @@
-﻿using testapp.Domain.Interfaces;
+using testapp.Domain.Interfaces;
 using testapp.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace testapp.Server.API 
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class MainReportController : ControllerBase
     {
         private readonly IMainReportService _service;
-
+        
         public MainReportController(IMainReportService service)
         {
             _service = service;
         }
-
+        
         [HttpGet("all")]
         public async Task<IActionResult> GetAllReports()
         {
             var reports = await _service.GetAllReportsAsync();
             return Ok(reports);
         }
-
+        
         [HttpPost("filter")]
         public async Task<IActionResult> GetReportsByDate([FromBody] ReportFilterDto filter)
         {
